f12e232dabb8a8a237218dcc09db96b94037ff0a
unknown <162107@SNKXW-JBAUGUES2.sn.corp>
2015-07-06 16:35:22 -0400

    [object Object]

--- gulpfile.js
+++ gulpfile.js
@@ -57,13 +57,11 @@ gulp.task('jshint-client', function() {
 });
 
 gulp.task('add-files', function() {
-    return gulp.src(['./*'])
-            .pipe(git.add());
+   return gulp.src('./*.*').pipe(git.add({args: '--all'}));
 });
 
 gulp.task('commit-files', function() {
-    return gulp.src(['./*'])
-            .pipe(git.commit('Making an update with ' + options.reason));
+   return gulp.src('./*.*').pipe(git.commit({args: '-m Making an update with ' + options.reason}));
 });
 
 gulp.task('push-files', function() {
